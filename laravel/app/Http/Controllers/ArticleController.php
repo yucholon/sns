7cd16d54b57202d505825d9ebc1173fb6e2a27a9
yucholon <hinweiswaqibcib@gmail.com>
2020-10-03 11:22:58 +0900

    はじめての更新

---
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+
+use App\Tag;
+
+use App\Http\Requests\ArticleRequest;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    //
+    public function __construct()
+    {
+      $this->authorizeResource(Article::class, 'article');
+    }
+
+
+    public function index()
+    {
+      $articles = Article::all()->sortByDesc('created_at');
+
+      return view('articles.index', ['articles' => $articles]);
+    }
+
+    public function create()
+    {
+      return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+      $article->fill($request->all());
+      $article->user_id = $request->user()->id;
+      $article->save();
+
+      $request->tags->each(function ($tagName) use ($article) {
+        $tag = Tag::firstOrCreate(['name' => $tagName]);
+        $article->tags()->attach($tag);
+      });
+
+      return redirect()->route('articles.index');
+    }
+
+    public function edit(Article $article)
+    {
+      return view('articles.edit', ['article' => $article]);
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+      $article->fill($request->all())->save();
+      return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article)
+    {
+      $article->delete();
+      return redirect()->route('articles.index');
+    }
+
+    public function show(Article $article)
+    {
+      return view('articles.show', ['article' => $article]);
+    }
+
+    public function like(Request $request, Article $article)
+    {
+      $article->likes()->detach($request->user()->id);
+      $article->likes()->attach($request->user()->id);
+
+      return [
+        'id' => $article->id,
+        'countLikes' => $article->count_likes,
+      ];
+    }
+
+    public function unlike(Request $request, Article $article)
+    {
+      $article->likes()->detach($request->user()->id);
+
+      return [
+        'id' => $article->id,
+        'countLikes' => $article->count_likes,
+      ];
+    }
+}
---
+++ laravel/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles','ArticleController')->except(['index','show'])->middleware('auth');
+Route::resource('/articles','ArticleController')->only(['show']);
+Route::prefix('articles')->name('articles.')->group(function() {
+  Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
+  Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
+});
